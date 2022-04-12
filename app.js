e0b4ff50bd645faff71ca05b3c4ec6a983254731
Jyothsna <94163611+JyothsnaVellampalli@users.noreply.github.com>
2022-04-12 06:24:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
+var dotenv = require('dotenv');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+require('dotenv').config()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
